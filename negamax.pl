e3c2e236b35bc404cdb87792e9ae42b19ae2963c
Hiidrel <hiidrel@home.localdomain>
2023-02-27 09:39:44 +0100

    first push, TP2 finished, TP1 to correct

---
+++ negamax.pl
@@ -0,0 +1,144 @@
+	/*
+	Ce programme met en oeuvre l'algorithme Minmax (avec convention
+	negamax) et l'illustre sur le jeu du TicTacToe (morpion 3x3)
+	*/
+	
+:- [tictactoe].
+
+
+	/****************************************************
+  	ALGORITHME MINMAX avec convention NEGAMAX : negamax/5
+  	*****************************************************/
+
+	/*
+	negamax(+J, +Etat, +P, +Pmax, [?Coup, ?Val])
+
+	SPECIFICATIONS :
+
+	retourne pour un joueur J donne, devant jouer dans
+	une situation donnee Etat, de profondeur donnee P,
+	le meilleur couple [Coup, Valeur] apres une analyse
+	pouvant aller jusqu'a la profondeur Pmax.
+.....................................
+	*/
+
+	/* Cas 1/ la profondeur maximale est atteinte : on ne peut pas
+	developper cet Etat ; 
+	il n'y a donc pas de coup possible a jouer (Coup = rien)
+	et l'evaluation de Etat est faite par l'heuristique. */
+negamax(J, Etat, P, P, [Coup, Val]) :- 
+	Coup = rien,
+	heuristique(J,Etat,Val).
+
+%*******************************************************************************
+	/* Cas 2/ la profondeur maximale n'est pas atteinte mais J ne
+	peut pas jouer ; au TicTacToe un joueur ne peut pas jouer
+	quand le tableau est complet (totalement instancie) ;
+	il n'y a pas de coup a jouer (Coup = rien)
+	et l'evaluation de Etat est faite par l'heuristique. */
+negamax(J, Etat, _P, _Pmax, [Coup, Val]) :- 
+	situation_terminale(J,Etat),
+	Coup = rien,
+	heuristique(J,Etat,Val).
+
+%*******************************************************************************
+	/* Cas 3/ la profondeur maxi n'est pas atteinte et J peut encore
+	jouer. Il faut evaluer le sous-arbre complet issu de Etat ; 
+
+	- on determine d'abord la liste de tous les couples
+	[Coup_possible, Situation_suivante] via le predicat
+	 successeurs/3 (deja fourni, voir plus bas).
+
+	- cette liste est passee a un predicat intermediaire :
+	loop_negamax/5, charge d'appliquer negamax sur chaque
+	Situation_suivante ; loop_negamax/5 retourne une liste de
+	couples [Coup_possible, Valeur]
+
+	- parmi cette liste, on garde le meilleur couple, c-a-d celui
+	qui a la plus petite valeur (cf. predicat meilleur/2);
+	soit [C1,V1] ce couple optimal. Le predicat meilleur/2
+	effectue cette selection.
+
+	- finalement le couple retourne par negamax est [Coup, V2]
+	avec : V2 is -V1 (cf. convention negamax vue en cours). */
+negamax(J, Etat, P, Pmax, [Coup, (Val)]) :-
+	P=<Pmax,
+	successeurs(J,Etat,Succ),
+	loop_negamax(J,P,Pmax,Succ,Liste_Couples),
+	meilleur(Liste_Couples,[Coup,V2]),
+	Val is -V2.
+
+%*******************************************************************************
+	/*******************************************
+	 DEVELOPPEMENT D'UNE SITUATION NON TERMINALE
+	 successeurs/3 
+	 *******************************************/
+
+	 /*
+   	 successeurs(+J,+Etat, ?Succ)
+
+   	 retourne la liste des couples [Coup, Etat_Suivant]
+ 	 pour un joueur donne dans une situation donnee 
+	 */
+
+successeurs(J,Etat,Succ) :-
+	copy_term(Etat, Etat_Suiv),
+	findall([Coup,Etat_Suiv],
+		    successeur(J,Etat_Suiv,Coup),
+		    Succ).
+
+	/*************************************
+         Boucle permettant d'appliquer negamax 
+         a chaque situation suivante :
+	*************************************/
+
+	/*
+	loop_negamax(+J,+P,+Pmax,+Successeurs,?Liste_Couples)
+	retourne la liste des couples [Coup, Valeur_Situation_Suivante]
+	a partir de la liste des couples [Coup, Situation_Suivante]
+	*/
+
+loop_negamax(_,_,_,[],[]).
+loop_negamax(J,P,Pmax,[[Coup,Suiv]|Succ],[[Coup,Vsuiv]|Reste_Couples]) :-
+	loop_negamax(J,P,Pmax,Succ,Reste_Couples),
+	adversaire(J,A),
+	Pnew is P+1,
+	negamax(A,Suiv,Pnew,Pmax,[_,Vsuiv]),
+	writeln(Reste_Couples),
+	writeln([Coup,Suiv]),
+	writeln(Vsuiv).
+
+%*******************************************************************************
+	/*********************************
+	 Selection du couple qui a la plus
+	 petite valeur V 
+	 *********************************/
+
+	/*
+	meilleur(+Liste_Couples, ?Meilleur_Couple)
+
+	SPECIFICATIONS :
+	On suppose que chaque element de la liste est du type [C,V]
+	- le meilleur dans une liste a un seul element est cet element
+	- le meilleur dans une liste [X|L] avec L \= [], est obtenu en comparant
+	  X et Y,le meilleur couple de L 
+	  Entre X et Y on garde celui qui a la petite valeur de V.
+	*/
+meilleur([X],X).
+meilleur([[CX,VX]|Liste_Couples], Meilleur_Couple):- 
+	Liste_Couples\=[],
+	meilleur(Liste_Couples,[CY,VY]),
+	(VX<VY-> Meilleur_Couple=[CX,VX]; Meilleur_Couple=[CY,VY]).
+
+
+%*******************************************************************************
+	/******************
+  	PROGRAMME PRINCIPAL
+  	*******************/
+
+main(C,V, Pmax) :-
+	Pmax=<9,
+	joueur_initial(J),
+	situation_initiale(S),
+	P is 0,
+	negamax(J, S, P, Pmax, [C, V]).
---
+++ tictactoe.pl
@@ -0,0 +1,183 @@
+/*********************************
+	DESCRIPTION DU JEU DU TIC-TAC-TOE
+	*********************************/
+
+	/*
+	Une situation est decrite par une matrice 3x3.
+	Chaque case est soit un emplacement libre (Variable LIBRE), soit contient le symbole d'un des 2 joueurs (o ou x)
+
+	Contrairement a la convention du tp precedent, pour modeliser une case libre
+	dans une matrice on n'utilise pas une constante speciale (ex : nil, 'vide', 'libre','inoccupee' ...);
+	On utilise plut�t un identificateur de variable, qui n'est pas unifiee (ex : X, A, ... ou _) .
+	La situation initiale est une "matrice" 3x3 (liste de 3 listes de 3 termes chacune)
+	o� chaque terme est une variable libre.	
+	Chaque coup d'un des 2 joureurs consiste a donner une valeur (symbole x ou o) a une case libre de la grille
+	et non a deplacer des symboles deja presents sur la grille.		
+	
+	Pour placer un symbole dans une grille S1, il suffit d'unifier une des variables encore libres de la matrice S1,
+	soit en ecrivant directement Case=o ou Case=x, ou bien en accedant a cette case avec les predicats member, nth1, ...
+	La grille S1 a change d'etat, mais on n'a pas besoin de 2 arguments representant la grille avant et apres le coup,
+	un seul suffit.
+	Ainsi si on joue un coup en S, S perd une variable libre, mais peut continuer a s'appeler S (on n'a pas besoin de la designer
+	par un nouvel identificateur).
+	*/
+
+situation_initiale([ [_,_,_],
+                     [_,_,_],
+                     [_,_,_] ]).
+
+situation([ [o,_,a],
+								[o,b,_],
+								[o,_,_] ]).
+
+	% Convention (arbitraire) : c'est x qui commence
+
+joueur_initial(x).
+
+
+	% Definition de la relation adversaire/2
+
+adversaire(x,o).
+adversaire(o,x).
+
+
+	/****************************************************
+	 DEFINIR ICI a l'aide du predicat ground/1 comment
+	 reconnaitre une situation terminale dans laquelle il
+	 n'y a aucun emplacement libre : aucun joueur ne peut
+	 continuer a jouer (quel qu'il soit).
+	 ****************************************************/
+
+situation_terminale(_Joueur, Situation) :-
+    ground(Situation).
+
+	/***************************
+	DEFINITIONS D'UN ALIGNEMENT
+	***************************/
+
+alignement(L, Matrix) :- ligne(L,Matrix).
+alignement(C, Matrix) :- colonne(C,Matrix).
+alignement(D, Matrix) :- diagonale(D,Matrix).
+
+	/********************************************
+	 DEFINIR ICI chaque type d'alignement maximal 
+ 	 existant dans une matrice carree NxN.
+	 ********************************************/
+	
+	/* alignement de lignes */
+
+ligne(L, M) :-
+	nth1(_E,M,L).
+
+	/* alignement de colonnes */
+
+transpose([[]|_], []).
+transpose(Matrix, [Row|Rows]) :- transpose_1st_col(Matrix, Row, RestMatrix),
+                                 transpose(RestMatrix, Rows).
+transpose_1st_col([], [], []).
+transpose_1st_col([[H|T]|Rows], [H|Hs], [T|Ts]) :- transpose_1st_col(Rows, Hs, Ts).
+
+colonne(C,M) :-
+	transpose(M,Mt),
+	ligne(C,Mt).
+
+	/* alignement de diagonales */
+		
+diagonale(D, M) :- 
+	premiere_diag(1,D,M).
+
+diagonale(D, M) :- 
+    seconde_diag(M,D).
+
+premiere_diag(_,[],[]).
+premiere_diag(K,[E|D],[Ligne|M]) :-
+	nth1(K,Ligne,E),
+	K1 is K+1,
+	premiere_diag(K1,D,M).
+
+seconde_diag(M, D) :-
+	length(M,K),
+	seconde_diag(K, M, D).
+
+seconde_diag(_,[],[]).
+seconde_diag(K,[Ligne|M],[E|D]) :-
+	nth1(K,Ligne,E),
+	K1 is K-1,
+	seconde_diag(K1,M,D).
+
+
+	/*****************************
+	 DEFINITION D'UN ALIGNEMENT 
+	 POSSIBLE POUR UN JOUEUR DONNE
+	 *****************************/
+
+possible([X|L],J) :- unifiable(X,J), possible(L,J).
+possible([],_).
+
+	/* Vérification du caractère unifiable des emplacements de la liste */
+
+unifiable(X,_) :- var(X).
+unifiable(X,J) :- ground(X), X=J.
+	
+	/**********************************
+	 DEFINITION D'UN ALIGNEMENT GAGNANT
+	 OU PERDANT POUR UN JOUEUR DONNE J
+	 **********************************/
+	/*
+	Un alignement gagnant pour J est un alignement
+possible pour J qui n'a aucun element encore libre.
+	*/
+		
+	/* Un alignement perdant pour J est un alignement gagnant pour son adversaire. */
+
+alignement_gagnant(Ali, J) :- ground(Ali), possible(Ali,J).
+
+alignement_perdant(Ali, J) :- adversaire(J,J2), alignement_gagnant(Ali,J2).
+
+	/* ****************************
+	DEFINITION D'UN ETAT SUCCESSEUR
+	****************************** */
+
+	/* 
+	Il faut definir quelle operation subit la matrice
+	M representant l'Etat courant
+	lorsqu'un joueur J joue en coordonnees [L,C]
+	*/	
+
+successeur(J, Etat,[L,C]) :- nth1(L,Etat,Lig), nth1(C,Lig,J).
+
+	/**************************************
+   	 EVALUATION HEURISTIQUE D'UNE SITUATION
+  	 **************************************/
+
+	/* 1/ l'heuristique est +infini si la situation J est gagnante pour J */
+
+heuristique(J,Situation,H) :-		% cas 1
+   H = 10000,				% grand nombre approximant +infini
+   alignement(Alig,Situation),
+   alignement_gagnant(Alig,J), !.
+	
+	/* 2/ l'heuristique est -infini si la situation J est perdante pour J */
+
+heuristique(J,Situation,H) :-		% cas 2
+   H = -10000,				% grand nombre approximant -infini
+   alignement(Alig,Situation),
+   alignement_perdant(Alig,J), !.	
+
+
+	/* 3/ sinon, on fait la difference entre :
+	   le nombre d'alignements possibles pour J
+	   -
+ 	   le nombre d'alignements possibles pour l'adversaire de J
+	On ne vient ici que si les cut precedents n'ont pas fonctionne, c-a-d si Situation n'est ni perdante ni gagnante. */
+
+heuristique(J,Situation,H) :-
+    % coups possibles pour J
+    findall(Alig_g,(alignement(Alig_g,Situation),possible(Alig_g,J)),Lg),
+    length(Lg,Cg),
+    % coups possibles pour l'adversaire
+    adversaire(J,J2),
+    findall(Alig_p,(alignement(Alig_p,Situation),possible(Alig_p,J2)),Lp),
+    length(Lp,Cp),
+    % H = nb de coups possibles pour J - nb de coups possibles pour J2
+    H is Cg-Cp.
